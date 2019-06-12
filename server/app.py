92b11065a03aaec32839a566e92cbe4ed015c5d4
adityavinodk <ads.vinodk@gmail.com>
2019-06-12 17:52:44 +0530

    Flask serving React

--- server/app.py
+++ server/app.py
@@ -1,17 +1,12 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='../frontend/build')
+app = Flask(__name__)
 
 # Serve React App
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
+@app.route('/')
+def my_index():
+    return render_template("index.html", token = "HELLO")
 
 
-if __name__ == '__main__':
-    app.run(use_reloader=True, port=5000, threaded=True)
+app.run(debug=True)
---
+++ server/static/react/precache-manifest.ccc78a7855187742e8e0e2d3baa3b77c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f90804628988c9e00dfc22ceaeb9b3",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "750c81f85b597c51c02b",
+    "url": "/static/react/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/react/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "750c81f85b597c51c02b",
+    "url": "/static/react/js/main.8c856ec7.chunk.js"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
